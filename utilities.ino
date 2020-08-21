add97d4c16a5a272ccdc0072ca2f658f2f8c9010
mhamilt <matt.hamilton@ed.ac.uk>
2020-08-21 16:20:44 +0100

    add wait for serial input function to aid debugging

--- utilities.ino
+++ utilities.ino
@@ -2,9 +2,10 @@
 void Serialprint64(uint64_t n)
 {
   print64(&Serial, n);
+  Serial.println();
 }
-
-size_t print64(Print* pr, uint64_t n) 
+//---------------------------------------------------------------------------------------------------------------------------------------
+size_t print64(Print* pr, uint64_t n)
 {
   char buf[21];
   char *str = &buf[sizeof(buf) - 1];
@@ -17,7 +18,7 @@ size_t print64(Print* pr, uint64_t n)
   pr->print(str);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------
-size_t print64(Print* pr, int64_t n) 
+size_t print64(Print* pr, int64_t n)
 {
   size_t s = 0;
   if (n < 0) {
@@ -26,12 +27,25 @@ size_t print64(Print* pr, int64_t n)
   }  return s + print64(pr, (uint64_t)n);
 }
 //---------------------------------------------------------------------------------------------------------------------------------------
-size_t println64(Print* pr, int64_t n) 
+size_t println64(Print* pr, int64_t n)
 {
   return print64(pr, n) + pr->println();
 }
 //---------------------------------------------------------------------------------------------------------------------------------------
-size_t println64(Print* pr, uint64_t n) 
+size_t println64(Print* pr, uint64_t n)
 {
   return print64(pr, n) + pr->println();
+}
+//---------------------------------------------------------------------------------------------------------------------------------------
+void waitForSerialInput()
+{
+  static const char *waitMessage = "Press SEND in Serial Monitor ";
+  int c = 0;
+  while (Serial.available() == 0)
+  {
+    Serial.print(waitMessage[c++]);
+    c %= 30;
+    delay(250);
+  }
+  Serial.println(".");
 }
